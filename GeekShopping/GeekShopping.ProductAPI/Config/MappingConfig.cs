bc7fd1ddc56c795f653033fb9ceedba35c833d0d
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2022-07-31 04:11:17 -0300

    create the initial product api structure

---
+++ GeekShopping/GeekShopping.ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
+
+namespace GeekShopping.ProductAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+           return new MapperConfiguration(config => config.CreateMap<ProductVO, Product>().ReverseMap());
+        }
+    }
+}
