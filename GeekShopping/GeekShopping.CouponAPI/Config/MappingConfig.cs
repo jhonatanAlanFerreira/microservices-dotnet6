d4103c52d8de76716584d2fa97f88759fcf5b911
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2022-08-17 00:11:58 -0300

    create the initial coupon api structure

---
+++ GeekShopping/GeekShopping.CouponAPI/Config/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GeekShopping.CouponAPI.Data.ValueObjects;
+using GeekShopping.CouponAPI.Model;
+
+namespace GeekShopping.CouponAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            return new MapperConfiguration(config =>
+            {
+               config.CreateMap<CouponVO, Coupon>().ReverseMap();
+            });
+        }
+    }
+}
