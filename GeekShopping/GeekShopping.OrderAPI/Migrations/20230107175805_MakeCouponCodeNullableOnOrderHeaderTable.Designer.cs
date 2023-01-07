54f9bbbd2af6b36e01f3757ca9f9057768735f31
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2023-01-07 15:03:42 -0300

    fix coupon code optional

--- GeekShopping/GeekShopping.OrderAPI/Migrations/20230107175805_MakeCouponCodeNullableOnOrderHeaderTable.Designer.cs
+++ GeekShopping/GeekShopping.OrderAPI/Migrations/20230107175805_MakeCouponCodeNullableOnOrderHeaderTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GeekShopping.OrderAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeekShopping.OrderAPI.Migrations
 {
     [DbContext(typeof(MySQLContext))]
-    partial class MySQLContextModelSnapshot : ModelSnapshot
+    [Migration("20230107175805_MakeCouponCodeNullableOnOrderHeaderTable")]
+    partial class MakeCouponCodeNullableOnOrderHeaderTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
