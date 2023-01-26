83f95b7f34aa0ba8f5dc7df7c51c54eec82219bd
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2023-01-26 01:09:31 -0300

    create numberOnlyTagHelper

---
+++ GeekShopping/GeekShopping.Web/TagHelpers/NumberOnlyTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GeekShopping.Web.TagHelpers
+{
+
+    [HtmlTargetElement("input", Attributes  = "number-only")]
+    public class NumberOnlyTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("onkeydown", "return (!(event.keyCode>=65) && event.keyCode!=32);");
+        }
+    }
+}
